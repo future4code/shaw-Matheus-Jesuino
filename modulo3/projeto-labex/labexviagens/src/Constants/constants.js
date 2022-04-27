9a0140bb3314416bb47965a966e62bde810b0c06
Matheus_Jesuino <matheusjesuino96@hotmail.com>
2022-04-27 16:57:59 -0300

    Imprementação da Lista de Viagens

---
+++ modulo3/projeto-labex/labexviagens/src/Constants/constants.js
@@ -0,0 +1,18 @@
+
+export const BASE_URL = "https://us-central1-labenu-apis.cloudfunctions.net/labeX/Matheus-Jesuino-Shaw/"
+
+
+export const Body = {
+    name: "",
+    age: "",
+    applicationText: "",
+    profession: "",
+    country: ""
+}
+
+export const headers = {
+    headers: {
+        ContentType: "application/json"
+    }
+}
+ 
--- modulo3/projeto-labex/labexviagens/src/Pages/ApplicationFormPage.js
+++ modulo3/projeto-labex/labexviagens/src/Pages/ApplicationFormPage.js
@@ -1,25 +1,53 @@
-import React from "react";
+import axios from "axios";
+import React, { useEffect, useState } from "react";
 import { useNavigate } from "react-router-dom";
-import {goToBack} from "../Coordinator/Coordinator"
+import { BASE_URL, Body, headers } from "../Constants/constants";
+import { goToBack } from "../Coordinator/Coordinator"
+import ListTripsPage from "./ListTripsPage";
 
 function ApplicationFormPage() {
     const navigate = useNavigate()
+    const [applyToTrip, setApplyToTrip] = useState({})
+
+    useEffect(() => {
+        apply()
+    }, [])
+
+    const apply = () => {
+
+        axios
+            .get(`${BASE_URL}trips/:id/apply`, Body, headers)
+            .then((res) => {
+                console.log(res.data);
+                setApplyToTrip({
+                    name: "",
+                    age: "",
+                    applicationText: "",
+                    profession: "",
+                    country: ""
+                })
+            })
+            .catch((err)=>{
+                alert(err.res)
+            })
+            }
 
 
+    return (
+        <div>
+            <select>
+                <input placeholder="Escolha sua Viagem"/>
+            </select>
+            <input type="text" placeholder="Nome" />
+            <input type="text" placeholder="Idade" />
+            <input type="text" placeholder="Texto de Candidatura" />
+            <input type="text" placeholder="Profissão" />
+            <input type="select" placeholder="Escolha seu Pais" />
+            <button onClick={() => goToBack(navigate)}>Voltar</button>
+            <button>Enviar</button>
 
-return (
-    <div>
-        <input type="select" placeholder="Escolha sua Viagem"/>
-        <input type="text" placeholder="Nome"/>
-        <input type="text" placeholder="Idade"/>
-        <input type="text" placeholder="Texto de Candidatura"/>
-        <input type="text" placeholder="Profissão"/>
-        <input type="select" placeholder="Escolha seu Pais"/>
-<button onClick= {()=>goToBack(navigate)}>Voltar</button>
-  <button>Enviar</button>
- 
-  </div>
-)
+        </div>
+    )
 }
 
 export default ApplicationFormPage
