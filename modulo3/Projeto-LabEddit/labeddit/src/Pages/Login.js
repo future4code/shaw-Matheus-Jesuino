9f03ad449fec768cdc9f83045f0c084bed2ea687
Matheus_Jesuino <matheusjesuino96@hotmail.com>
2022-05-09 17:14:49 -0300

    Rotas Feitas

---
+++ modulo3/Projeto-LabEddit/labeddit/src/Pages/Login.js
@@ -0,0 +1,23 @@
+import React from "react";
+import axios from "axios";
+import styled from "styled-components";
+import {useNavigate } from "react-router-dom";
+import {goToCadastro, goToFeed} from "../Routes/Coordinator"
+
+
+export const Login = () => {
+    const navigate = useNavigate()
+
+
+    return (
+        <div>
+            <h1>Login</h1>
+            <input type="email" placeholder="Email" />   
+            <input type="password" placeholder="Senha" />     
+            <button onClick = {()=> goToFeed(navigate)}>Entrar</button>     
+            <button onClick = {()=> goToCadastro(navigate)}>Cadastre-se</button> 
+        </div>
+    );
+}
+
+export default Login;
---
+++ modulo3/Projeto-LabEddit/labeddit/src/Routes/Coordinator.js
@@ -0,0 +1,15 @@
+export const  goToFeed = (navigate) => {
+    navigate("/")
+}
+
+export const goToCadastro = (navigate) => {
+    navigate("/cadastro")
+}
+
+export const goToLogin = (navigate) => {
+    navigate("/login")
+}
+
+export const goToPost = (navigate) => {
+    navigate("/post")
+}
