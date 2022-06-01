12349d8c963b0a02044f9bf984ae5c6b54e0305e
Matheus_Jesuino <matheusjesuino96@hotmail.com>
2022-06-01 14:24:18 -0300

    Exercicio Iniciado

---
+++ modulo4/fluxo-de-dados-backend/src/index.ts
@@ -0,0 +1,13 @@
+console.log("Hello, Word");
+
+// import express from "express";
+// import cors from "cors";
+
+// const app = express();
+
+// app.use(express.json());
+// app.use(cors());
+
+// app.listen(3003, () => {
+//     console.log("Server is running in http://localhost:3003")
+// });
