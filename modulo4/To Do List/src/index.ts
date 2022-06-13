1de9d0bca252d0f127a2c0ec5066609a1b4bd21d
Matheus_Jesuino <matheusjesuino96@hotmail.com>
2022-06-13 13:47:33 -0300

    Projeto sendo feito

--- modulo4/To Do List/src/index.ts
+++ modulo4/To Do List/src/index.ts
@@ -1,6 +1,7 @@
-import express, { Express } from "express";
+import express, { Express, Response, Request } from "express";
 import cors from "cors";
-import { AddressInfo } from "net";
+
+
 
 
 
@@ -8,11 +9,9 @@ const app: Express = express();
 app.use(express.json());
 app.use(cors());
 
-const server = app.listen(process.env.PORT || 3003, () => {
-    if (server) {
-       const address = server.address() as AddressInfo;
-       console.log(`Server is running in http://localhost: ${address.port}`);
-    } else {
-       console.error(`Failure upon starting server.`);
-    }
-});
+
+
+app.listen(3003,()=>{
+   console.log("Servidor rodando na porta 3003");
+   
+})
